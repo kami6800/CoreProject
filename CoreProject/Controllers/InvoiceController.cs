0086bb015a9d11fed5b1689d1781fdd2069213e4
Kamilla Bilet Høst <kami6800@edu.campusvejle.dk>
2020-10-07 09:13:07 +0200

    Add project files.

---
+++ CoreProject/Controllers/InvoiceController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoiceController : ControllerBase
+    {
+        NorthwindContext context = new NorthwindContext();
+
+        //[Route("api/{controller=invoice}/incomplete/{id}")]
+        //[HttpGet("{id}")]
+        //public string GetIncompletedOrders(string id)
+        //{
+        //    //return context.Orders.Where(order => order.CustomerId == id).Where(order => order.ShippedDate != null).ToList();
+        //    return "hi";
+        //}
+
+        //[Route("api/[controller]/incomplete/")]
+        //[HttpGet]
+        //public List<Orders> GetIncompletedOrders()
+        //{
+        //    return context.Orders.ToList();
+        //}
+
+        [Route("api/[controller]/incomplete/")]
+        [HttpGet]
+        public string GetIncompletedOrders()
+        {
+            return "context.orders.tolist();";
+        }
+
+        // GET: api/Invoice
+        [HttpGet]
+        public List<Invoices> Get()
+        {
+            return context.Invoices.ToList();
+        }
+
+        // GET: api/Invoice/5
+        [HttpGet("{id}", Name = "Get")]
+        public List<Invoices> Get(string id)
+        {
+            return context.Invoices.Where(invoice => invoice.CustomerId == id).ToList();
+        }
+
+        // POST: api/Invoice
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Invoice/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CoreProject/Models/ProductsAboveAveragePrice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoreProject.Models
+{
+    public partial class ProductsAboveAveragePrice
+    {
+        public string ProductName { get; set; }
+        public decimal? UnitPrice { get; set; }
+    }
+}
